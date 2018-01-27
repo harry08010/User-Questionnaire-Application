a9debfeb53c448d5ca4af75aca0c11814c33c01d
harry08010 <harry08010@tamu.edu>
2018-01-27 14:29:28 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express'); // import in the express library (common JS module in server side)
+const app = express(); // generate a new application representing a running express app (listen to request and route to Routes)
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+}); // a route handler
+
+const PORT = process.env.PORT || 5000; // const name is capitalized so that the value should not be changed lightly!
+// if the port number is not defined by heroku, use 5000 by default for development.
+app.listen(PORT); 
+
+// in terminal: node index.js
+// in browser: localhost:5000
