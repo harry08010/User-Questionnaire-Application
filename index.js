6b3c68d6406018904eac1e16cc2d07aafa1ddaa2
harry08010 <harry08010@tamu.edu>
2018-02-02 22:01:20 -0600

    cookie session and logout

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express'); // import in the express library (common JS 
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
+const cookieSession = require('cookie-session'); // access to cookies
+const passport = require('passport'); // tell passport to make use of cookies
 require('./models/User');
 require('./services/passport'); // the order of "require" is important, first define model then use it
 
 mongoose.connect(keys.mongoURI);
 
 const app = express(); // generate a new application representing a running express app (listen to request and route to Routes)
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]  // encrypt the cookie
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 // require('./routes/authRoutes')(app);
