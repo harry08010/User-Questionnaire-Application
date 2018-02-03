8b08430400ca8313c9093fce7408c9167e029a5b
harry08010 <harry08010@tamu.edu>
2018-02-03 03:01:19 -0600

    dev and prod

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// dev credentials or prod credentials control
+if (process.env.NODE_ENV === 'production') {
+    // production
+    module.exports = require('./prod');
+} else {
+    // development
+    module.exports = require('./dev');
+}
