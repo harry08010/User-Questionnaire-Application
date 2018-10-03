1b254f739d251d80bad9e395bd1f20c9201e9635
harry08010 <harry08010@tamu.edu>
2018-10-02 22:27:56 -0500

    login logout fix

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-    render() {
-        return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="#" className="left brand-logo">QeedBack</a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Login With Google</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">JavaScript</a></li>
-                    </ul>
-                </div>
-            </nav>
-        )
-        
-    };
+  renderContent () {
+    switch (this.props.auth) {
+    case null:
+      return;
+    case false:
+      return (
+        <li><a href="/auth/google">Login With Google</a></li>
+      );
+    default:
+      return (
+        <li><a href="/api/logout">Logout</a></li>
+      );
+    }
+  }
+  render () {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to = { this.props.auth ? '/surveys' : '/' }
+            className="left brand-logo"
+          >
+            QeedBack
+          </Link>
+          <ul className="right">
+            { this.renderContent() }
+          </ul>
+        </div>
+      </nav>
+    );
+  };
 }
 
-export default Header;
+function mapStateToProps (state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps)(Header);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Queedback
+      </h1>
+      <b>Collect feedback from your users</b>
+    </div>
+  );
+};
+
+export default Landing;
